274456a1c3b10122b0bd577813409ff2e4657c6a
Abdelillahelasri1 <elasriabdelilah68@gmail.com>
2022-11-18 15:44:25 +0100

    login pages

---
+++ sign-up.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="main.js"></script>
+</head>
+<body>
+    <!--login section start-->
+    <div class="container">
+        <div class="center">
+            <h1>sign up</h1>
+            <div class="inp">
+                <input type="email" placeholder="  Email">
+                <input type="text" placeholder="UserName">
+                <input type="password" placeholder="  password">
+                <button>sign up</button>
+            </div>
+            <p>if you have account <span><a href="login-pages.php">Login in !</a></span></p>
+        </div>
+    </div>
+    <!--login section end-->
+</body>
+</html>
